f6da8e8de5da224a79a0b5ef9a1a9ba9c403a807
thegalaxydev <jojomalrbx@gmail.com>
2024-10-26 02:37:35 +0000

    Removed temp files.

--- Aloha/temp/114e876d-2f5a-48e5-b579-da11e83270a0.lua
+++
@@ -1,8 +0,0 @@
- 
-	task = require("@lune/task")
-	require = function(req) error("require is disabled.") end
-	getfenv = function() error("getfenv is disabled.") end
-	setfenv = function() error("setfenv is disabled.") end
-
-	print("T")
-	
