ca8429dbcd1804cbb0f9385b474e67fa8ca19f36
thegalaxydev <jojomalrbx@gmail.com>
2024-10-30 17:54:29 +0000

    Updated README.md

--- install.ps1
+++ install.ps1
@@ -4,9 +4,7 @@ $luneUrl = "https://github.com/lune-org/lune/releases/download/v0.8.9/lune-0.8.9
 $styluaUrl = "https://github.com/JohnnyMorganz/StyLua/releases/download/v0.20.0/stylua-windows-x86_64.zip"
 
 $luneZipPath = "$PSScriptRoot\lune-0.8.9-windows-x86_64.zip"
-$styluaZipPath = "$PSScriptRoot\stylua-windows-x86_64.zip"
 $luneExtractPath = "$PSScriptRoot\"
-$styluaExtractPath = "$PSScriptRoot\"
 
 Write-Output "Downloading Lune from $luneUrl..."
 Invoke-WebRequest -Uri $luneUrl -OutFile $luneZipPath
@@ -21,14 +19,3 @@ Write-Output "Lune has been downloaded and unzipped to $luneExtractPath."
 # Run 'lune setup'
 Write-Output "Running 'lune setup'..."
 & "$luneExtractPath\lune.exe" setup
-
-Write-Output "Downloading StyLua from $styluaUrl..."
-Invoke-WebRequest -Uri $styluaUrl -OutFile $styluaZipPath
-
-Write-Output "Unzipping StyLua..."
-Expand-Archive -Path $styluaZipPath -DestinationPath $styluaExtractPath -Force
-
-Remove-Item -Path $styluaZipPath
-
-Write-Output "StyLua has been downloaded and unzipped to $styluaExtractPath."
-Write-Output "Installation complete! Lune and StyLua are ready for use."
